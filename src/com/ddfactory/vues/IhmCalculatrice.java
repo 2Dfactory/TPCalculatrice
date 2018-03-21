475d7f797fa48866d5774ad3519c349ef2636a4a
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-03-21 16:57:07 +0100

    Ajout des bordures

--- src/com/ddfactory/vues/IhmCalculatrice.java
+++ src/com/ddfactory/vues/IhmCalculatrice.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -22,10 +23,9 @@ public class IhmCalculatrice extends JFrame implements ActionListener{
 	
 	private JLabel label = new JLabel();
 	private JPanel labelPane = new JPanel();
+	//private JPanel centerPane = new JPanel();
 	private JPanel buttonArea1 = new JPanel();
 	private JPanel buttonArea2 = new JPanel();
-	private JPanel contentPane = new JPanel();
-	
 	private String[] listOfButtonA1 = {"1","2","3","4","5","6","7","8","9","0",".","="};
 	private String[] listOfButtonA2 = {"C","+","-","*","/"};
 	private String str ="";
@@ -51,20 +51,21 @@ public class IhmCalculatrice extends JFrame implements ActionListener{
 		
 		//Initialisation des layouts
 		GridLayout glA1 = new GridLayout(4, 3);
-		glA1.setHgap(10);
-		glA1.setVgap(10);
+		glA1.setHgap(5);
+		glA1.setVgap(5);
 		
 		GridLayout glA2 = new GridLayout(5, 1);
-		glA2.setHgap(10);
-		glA2.setVgap(1);
+		glA2.setHgap(5);
+		glA2.setVgap(5);
 		
 		buttonArea1.setLayout(glA1);
 		//buttonArea1.setPreferredSize(new Dimension((int)(size.getWidth()-100), (int)(size.getHeight()-40)));
 		buttonArea1.getInsets();
-		
+		buttonArea1.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 0));
 		
 		buttonArea2.setLayout(glA2);
-		buttonArea2.setPreferredSize(new Dimension((int)(size.getWidth()-230), (int)(size.getHeight()-40)));
+		buttonArea2.setPreferredSize(new Dimension((int)(size.getWidth()-200), (int)(size.getHeight()-40)));
+		buttonArea2.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 10));
 		
 		addButtonToPanel(listOfButtonA1, buttonArea1);
 		addButtonToPanel(listOfButtonA2, buttonArea2);
@@ -75,9 +76,25 @@ public class IhmCalculatrice extends JFrame implements ActionListener{
 		contentLayout.setVgap(10);
 		this.setLayout(contentLayout);
 		
+		//BorderLayout bdl = new BorderLayout();
+		//bdl.setHgap(10);
+		//bdl.setVgap(10);
+		
+		//centerPane.setLayout(bdl);
+		//centerPane.add(labelPane, BorderLayout.NORTH);
+		
+		//centerPane.add(buttonArea1, BorderLayout.CENTER);
+		//centerPane.add(buttonArea2, BorderLayout.EAST);
+		
+		
+		
 		this.getContentPane().add(labelPane, BorderLayout.NORTH);
 		this.getContentPane().add(buttonArea1, BorderLayout.CENTER);
 		this.getContentPane().add(buttonArea2, BorderLayout.EAST);
+		
+		
+		
+		//this.getContentPane().add(centerPane, BorderLayout.CENTER);
 
 /*		this.contentPane.setPreferredSize(new Dimension((int)(size.getWidth()-40),(int)(size.getHeight()-40)));
 		this.contentPane.setLayout(new GridBagLayout());
