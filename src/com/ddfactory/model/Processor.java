43cea1eb16fcfceff74a26c40d56ac9e09d9a475
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-03-22 16:53:00 +0100

    Màj de la Classe Processor

--- src/com/ddfactory/model/Processor.java
+++ src/com/ddfactory/model/Processor.java
@@ -9,17 +9,63 @@ public class Processor implements Observable{
 
 	private ArrayList<Observateur> listObs = new ArrayList<Observateur>();
 	private String texte = "";
+	private boolean operateur = false;
+	private double op1 = 0, op2 = 0;
+	private String typeOperateur = "";
+	
 	
 	public void calculate(String str) {
 		
-		switch(str) {
-		case "c": this.texte ="";
-				break;
-		default:this.texte+=str;
-			
+
+		if(str.equalsIgnoreCase("c")) {
+			this.texte = "";
+			this.updateObservateur();
+			return;
 		}
 		
+		if(str.equalsIgnoreCase("=")) {
+			resultatOperation(op1, op2, typeOperateur);
+			this.operateur = false;
+			this.updateObservateur();
+			return;
+		}
+		
+		if(str.equalsIgnoreCase("+") || str.equalsIgnoreCase("-") || str.equalsIgnoreCase("/") || str.equalsIgnoreCase("*")) {
+			
+			typeOperateur = str.toLowerCase();
+			if(this.operateur) {
+				op1 = resultatOperation(op1, op2, typeOperateur);
+			}
+			this.operateur = true;
+		}
+		
+		this.texte+=str;
+		
+		
+//		switch(str) {
+//		case "c": this.texte = "";
+//				break;
+//		case "+" : this.operateur = true;
+//				this.typeOperateur = str;
+//		default : this.texte+=str;
+//		}
+		
+		
+		
+		
+		
+		
+		
+
+		
 		this.updateObservateur();
+		
+	}
+	
+	private double resultatOperation(double op1, double op2, String operateur) {
+		
+		
+		return 0;
 		
 	}
 	
